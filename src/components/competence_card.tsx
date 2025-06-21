ff2059ecab39e722cf87f803f74c741a4748aaa0
Pierrino421 <pierrinokavege@gmail.com>
2025-06-21 23:36:22 +0200

    maj1

--- src/components/competence_card.tsx
+++ src/components/competence_card.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
+import React from "react";
 
 interface ProjectProps {
   title: string;
   linkimg: string;
-  
-  
 }
 
-const CardC: React.FC<ProjectProps> = ({ title,linkimg }) => {
+const CardC: React.FC<ProjectProps> = ({ title, linkimg }) => {
   return (
-  
-         <div className="bg-white rounded-lg p-5 w-40 h-40 shadow hover:shadow-lg transition flex flex-col items-center justify-center">
-      <div className="w-25 h-25 mb-3">
-        <img
-          src={linkimg}
-          alt={title}
-          className="w-full h-full object-contain"
-        />
-      </div>
-      <h4 className="text-sm font-medium text-gray-900 text-center">{title}</h4>
-    </div>
-         
+    <div className="bg-white rounded-lg p-5 shadow hover:shadow-lg transition flex flex-col items-center justify-center w-full max-w-[160px] aspect-square">
+  <div className="w-16 h-16 mb-3">
+    <img
+      src={linkimg}
+      alt={title}
+      className="w-full h-full object-contain"
+    />
+  </div>
+  <h4 className="text-sm font-medium text-gray-900 text-center">{title}</h4>
+</div>
+
   );
 };
 
